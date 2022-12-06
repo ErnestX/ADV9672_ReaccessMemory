9fece9336a4d3a2bf393e3f97334030254cc5b63
ErnestX <xiangspecial2012@gmail.com>
2022-12-06 12:21:13 -0500

    line weight and color

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-import { DensityContours } from "./contour.js";
 import { MemoryContours } from "./contour.js";
 
-var eruptions = [{x: 79, y: 3.6}, {x: 54, y: 1.8}, {x: 74, y: 3.333}, {x: 62, y: 2.283}, {x: 85, y: 4.533}, {x: 55, y: 2.883}, {x: 88, y: 4.7}, {x: 85, y: 3.6}, {x: 51, y: 1.95}, {x: 85, y: 4.35}, {x: 54, y: 1.833}, {x: 84, y: 3.917}, {x: 78, y: 4.2}, {x: 47, y: 1.75}, {x: 83, y: 4.7}, {x: 52, y: 2.167}, {x: 62, y: 1.75}, {x: 84, y: 4.8}, {x: 52, y: 1.6}, {x: 79, y: 4.25}];
-
 MemoryContours();
-
-// $('#testing').on('click',function(){ alert("Hello! I am an alert box!!"); });
-
-DensityContours(eruptions, {
-  x: d => d.x,
-  y: d => d.y,
-  xLabel: "Idle (min.) ",
-  yLabel: "Erupting (min.)",
-  width: 600,
-  height: 600,
-  stroke: "steelblue",
-  fill: "white"
-})
--- js/contour.js
+++ js/contour.js
@@ -33,86 +33,22 @@ export function MemoryContours() {
       currentPoints[i][0] += (basePointsAverage[0] - currentPoints[i][0]) / numOfEpisodes;
       currentPoints[i][1] += (basePointsAverage[1] - currentPoints[i][1]) / numOfEpisodes;
     }
+    var lc = 175 + 80 / numOfEpisodes * e;
+    var lineColor = rgb(lc,lc,lc);
+    var lineWidth = 1.2 + 0.6 / numOfEpisodes * e;
     svg
     .append('path')
     .on("click", function(){
-      alert("Hello! I am an alert box #".concat(e.toString()));
+      alert("Clicked on contour #".concat(e.toString()));
     })
     .attr('d', curve(currentPoints))
     .attr("stroke-linejoin", "round")
-    .attr("fill", "white")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1);
+    .attr("fill", "black")
+    .attr("stroke", lineColor)
+    .attr("stroke-width", lineWidth);
   }
 }
 
-export function DensityContours(data, {
-  x = ([x]) => x, // given d in data, returns the (quantitative) x-value
-  y = ([, y]) => y, // given d in data, returns the (quantitative) y-value
-  xLabel, // a label for the x-axis
-  yLabel, // a label for the y-axis
-  bandwidth = 30, // the bandwidth for contouring (in pixels)
-  thresholds = 30, // requested number of contours
-  fill = "none", // the fill color (either a constant or a function of the density)
-  stroke = typeof fill === "function" ? null : "currentcolor", // stroke color (either a constant or a function of the density)
-  strokeWidth = (d, i) => i % 5 ? 0.25 : 1, // stroke width as a function of the density
-  marginTop = 20, // top margin, in pixels
-  marginRight = 30, // right margin, in pixels
-  marginBottom = 30, // bottom margin, in pixels
-  marginLeft = 40, // left margin, in pixels
-  width = 640, // outer width, in pixels
-  height = 400, // outer height, in pixels
-  xDomain, // [xmin, xmax]
-  xRange = [marginLeft, width - marginRight], // [left, right]
-  yDomain, // [ymin, ymax]
-  yRange = [height - marginBottom, marginTop] // [bottom, top]
-} = {}) {
-
-  //console.log("creating contour");
-
-  // Compute values.
-  const X = d3.map(data, x);
-  const Y = d3.map(data, y);
-  const I = d3.range(data.length);
-
-  const svg = d3.select("#SvgContour0")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", [0, 0, width, height])
-    .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
-
-  if (xDomain === undefined) xDomain = d3.extent(X);
-  if (yDomain === undefined) yDomain = d3.extent(Y);
-
-  const xScale = d3.scaleLinear()
-    .domain(xDomain).nice()
-    .rangeRound(xRange);
-
-  const yScale = d3.scaleLinear()
-    .domain(yDomain).nice()
-    .rangeRound(yRange);
-
-  const contours = d3.contourDensity()
-    .x(i => xScale(X[i]))
-    .y(i => yScale(Y[i]))
-    .size([width, height])
-    .bandwidth(bandwidth)
-    .thresholds(thresholds)
-    (I);
-
-console.log(contours);
-
-  svg.append("g")
-    .attr("stroke-linejoin", "round")
-    .attr("fill", typeof fill === "function" ? null : fill)
-    .attr("stroke", typeof stroke === "function" ? null : stroke)
-    .attr("stroke-width", typeof strokeWidth === "function" ? null : strokeWidth)
-    .selectAll("path")
-    .data(contours)
-    .join("path")
-    .attr("fill", typeof fill === "function" ? d => fill(d.value) : null)
-    .attr("stroke", typeof stroke === "function" ? d => stroke(d.value) : null)
-    .attr("stroke-width", typeof strokeWidth === "function" ? (d, i) => strokeWidth(d.value, i) : null)
-    .attr("d", d3.geoPath());
+function rgb(r, g, b){
+  return ["rgb(",r,",",g,",",b,")"].join("");
 }
